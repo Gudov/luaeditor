ac77b62b48c8b8f491ca9e4e5b79150002906a6d
Gudov <dima@novator.su>
2020-04-27 06:07:57 +0700

    add file selection dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+	connect(this->ui->actionOpen, &QAction::triggered, this, &MainWindow::openFileInput);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +17,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::openFileInput()
+{
+	QString fileName = QFileDialog::getOpenFileName(this,
+		tr("Open lua"), "C:\\war2", tr("*.lua"));
+
+	QFile file(fileName);
+	if (file.exists()) {
+		this->openLua(file);
+	}
+}
+
+void MainWindow::openLua(QFile &file)
+{
+
+}
